36970477387db7b5cac2f55f84a1dd2a017e986e
sserges <agounkeserge@gmail.com>
2020-09-19 11:07:18 +0000

    Delete todo method added

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,55 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-
-import Todos from './components/Todos';
+import Todos from "./components/Todos";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Todos
+    Todos,
   },
   data() {
     return {
       todos: [
         {
           id: 1,
-          title: 'Todo One',
-          completed: false
+          title: "Todo One",
+          completed: false,
         },
         {
           id: 2,
-          title: 'Todo Two',
-          completed: true
+          title: "Todo Two",
+          completed: true,
         },
         {
           id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
-    }
-  }
-}
+          title: "Todo Three",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+  },
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
 </style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <div v-for="todo in todos" v-bind:key="todo.id">
-            <h3>{{ todo.title }}</h3>
-        </div>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
     </div>
+  </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
+
 export default {
-    name: "Todos",
-    props: ["todos"]
-}
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem,
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
